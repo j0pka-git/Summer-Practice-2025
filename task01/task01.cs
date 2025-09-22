54361a38d4ec08a0fa00e32a0db0925d7c345717
Байбулов Ильяс <kuzenbaev2006@gmail.com>
2025-09-22 23:22:25 +0600

    string extension method implementation

---
+++ task01/task01.cs
@@ -0,0 +1,37 @@
+﻿namespace task01;
+
+public static class StringExtensions
+{
+    public static bool IsPalindrome(this string input)
+    {
+        if (input.Length > 0)
+        {
+            string new_input = input.ToLower();
+            string input1 = "";
+            string input2 = "";
+            foreach (var ch in new_input)
+            {
+                if (!char.IsPunctuation(ch) && !char.IsWhiteSpace(ch))
+                {
+                    input1 += ch;
+                }
+            }
+            for (int i = input1.Length - 1; i >= 0; i--)
+            {
+                input2 += input1[i];
+            }
+            if (input1 == input2)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        else
+        {
+            return false;
+        }
+    }
+}
