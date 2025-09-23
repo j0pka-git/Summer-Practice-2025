48d88ed652afe94a5c45ad14f9d83e11e5af0ff1
Байбулов Ильяс <kuzenbaev2006@gmail.com>
2025-09-23 22:32:43 +0600

    implementation of DirectorySizeCommand

---
+++ FileSystemCommands/FileSystemCommands.cs
@@ -0,0 +1,43 @@
+﻿using CommandLib;
+
+namespace FileSystemCommands;
+
+public class DirectorySizeCommand : ICommand
+{
+    private string DirectoryName;
+    public long Size { get; private set; }
+    public DirectorySizeCommand(string directoryName)
+    {
+        DirectoryName = directoryName;
+        Size = 0;
+    }
+    public void Execute()
+    {
+        if (Directory.Exists(DirectoryName))
+        {
+            foreach (var file in Directory.GetFiles(DirectoryName, "*", SearchOption.AllDirectories))
+            {
+                Size += new FileInfo(file).Length;
+            }
+        }
+    }
+}
+
+public class FindFilesCommand : ICommand
+{
+    private string DirectoryName;
+    private string Mask;
+    public string[] Files { get; set; } = new string[0];
+    public FindFilesCommand(string directoryName, string mask)
+    {
+        DirectoryName = directoryName;
+        Mask = mask;
+    }
+    public void Execute()
+    {
+        if (Directory.Exists(DirectoryName))
+        {
+            Files = Directory.GetFiles(DirectoryName, Mask);
+        }
+    }
+}
