6d4d7a2826ad2ca33ab63ef23064c50de50c5308
Байбулов Ильяс <kuzenbaev2006@gmail.com>
2025-09-23 14:38:17 +0600

    implementation of the space battle game

---
+++ task04/task04.cs
@@ -0,0 +1,76 @@
+﻿namespace task04;
+
+public interface ISpaceship
+{
+    void MoveForward();      
+    void Rotate(int angle);  
+    void Fire();           
+    int Speed { get; }       
+    int FirePower { get; }  
+}
+
+public class Cruiser : ISpaceship
+{
+    public int Speed { get; }
+    public int FirePower { get; }
+    public double X;
+    public double Y;
+    public double Angle;
+    public int Damage;
+    public Cruiser()
+    {
+        Speed = 50;
+        FirePower = 100;
+        X = 0;
+        Y = 0;
+        Angle = 0;
+        Damage = 0;
+    }
+    public void MoveForward()
+    {
+        double radian = Angle * (Math.PI / 180);
+        X += Speed * Math.Cos(radian);
+        Y += Speed * Math.Sin(radian); 
+    }
+    public void Rotate(int angle)
+    {
+        Angle = (Angle + angle) % 360;
+    }
+    public void Fire()
+    {
+        Damage += 100;
+    }
+}
+
+public class Fighter : ISpaceship
+{
+    public int Speed { get; }
+    public int FirePower { get; }
+    public double X;
+    public double Y;
+    public double Angle;
+    public int Damage;
+    public Fighter()
+    {
+        Speed = 100;
+        FirePower = 50;
+        X = 0;
+        Y = 0;
+        Angle = 0;
+        Damage = 0;
+    }
+    public void MoveForward()
+    {
+        double radian = Angle * (Math.PI / 180);
+        X += Speed * Math.Cos(radian);
+        Y += Speed * Math.Sin(radian); 
+    }
+    public void Rotate(int angle)
+    {
+        Angle = (Angle + angle) % 360;
+    }
+    public void Fire()
+    {
+        Damage += 50;
+    }
+}
